75664ab6ef17f7c200459ecb700fb72e3b275f9f
davidcph0512 <38375910+davidcph0512@users.noreply.github.com>
2024-04-12 22:44:00 +0800

    Fix: value set is undefined

--- js-native-input-value.js
+++ js-native-input-value.js
@@ -1,11 +1,10 @@
 HTMLElement.prototype.nativeValue = function(value) {
-  const valueSetter = Object.getOwnPropertyDescriptor(this, 'value').set;
-  const prototype = Object.getPrototypeOf(this);
-  const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
-  if (valueSetter && valueSetter !== prototypeValueSetter) {
-    prototypeValueSetter.call(this, value);
-  } else {
-    valueSetter.call(this, value);
-  }
-  this.dispatchEvent(new Event('input', { bubbles: true }));
+  const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+    window.HTMLInputElement.prototype,
+    'value').set;
+  nativeInputValueSetter.call(this, value);
+  const event = new Event('input', { bubbles: true });
+  this.dispatchEvent(event)
 }
+
+
