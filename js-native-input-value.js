4d03c4bc91a14900076732ab410f9257d39e6466
davidcph0512 <38375910+davidcph0512@users.noreply.github.com>
2024-04-12 22:53:44 +0800

    Fix: Illegal invocation

--- js-native-input-value.js
+++ js-native-input-value.js
@@ -1,10 +1,12 @@
-HTMLElement.prototype.nativeValue = function(value) {
-  const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
-    window.HTMLInputElement.prototype,
-    'value').set;
-  nativeInputValueSetter.call(this, value);
-  const event = new Event('input', { bubbles: true });
-  this.dispatchEvent(event)
-}
+document.addEventListener('DOMContentLoaded', () => {
+  HTMLElement.prototype.nativeValue = function(value) {
+    const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+      window.HTMLInputElement.prototype,
+      'value').set;
+    nativeInputValueSetter.call(this, value);
+    const event = new Event('input', { bubbles: true });
+    this.dispatchEvent(event)
+  }
+})
 
 
